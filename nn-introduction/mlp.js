afdbad78cca5631e2b1966ff6539f0cd630ab51d
CalvinGaiennie <calvingaiennie@gmail.com>
2025-01-05 14:18:16 -0600

    a

--- nn-introduction/mlp.js
+++ nn-introduction/mlp.js
@@ -4,7 +4,7 @@ class MLP {
       [0.5, 0.5, 0.5, 0.5],
       //Weights for hidden neuron 1
       [-0.5, -0.5, -0.5, -0.5],
-      //Weights for hidden neuron 2
+      //Weights for hidden neuron 2...
     ]),
       (this.biasesHidden = [0.1, -0.1]),
       (this.weightsHiddenOutput = [
